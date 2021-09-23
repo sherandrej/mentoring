8151b27edae985bcdc78dcf3caa807419ad008d4
andrey.sherstnev <andrey.sherstnev@intexsoft.by>
2021-09-23 13:07:44 +0300

    new changes

--- var_log.sh
+++ var_log.sh
@@ -1,6 +1,6 @@
- #!/bin/bash
- ls -la /var/log
- PAUSE=5
- echo "Waiting $PAUSE sec"
- read -t  $PAUSE
- exit 0
+#!/bin/bash
+ls -la /var/log
+PAUSE=5
+echo "Waiting $PAUSE sec"
+read -t  $PAUSE
+exit 0
