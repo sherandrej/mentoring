d1fede34c9202d2cb42c865789bf884ac7e62bf3
andrey.sherstnev <andrey.sherstnev@intexsoft.by>
2021-09-28 11:15:34 +0300

    output_files and new branch

--- sys_log.sh
+++ sys_log.sh
@@ -9,13 +9,13 @@ echo "$TODAY_DATE"
 if [[ -f "$FILEPATH" ]]
   then
 	  echo "First 10 lines of $FILEPATH ************************"
-		tail -n 10 $FILEPATH
+		tail -n 10 $FILEPATH > out_logs/first10_syslog.txt
 		echo "Last 10 lines of $FILEPATH ************************"
-		head -n 10 $FILEPATH
+		head -n 10 $FILEPATH > out_logs/last10_syslog.txt
 		echo "Todays lines of $FILEPATH ####################"
-		grep -c 10 "$TODAY_DATE" "$FILEPATH"
+		grep -c 10 "$TODAY_DATE" "$FILEPATH" > out_logs/today10_syslog.txt
 		echo "CRON lines of $FILEPATH ####################"
-		grep -wi "CRON" "$FILEPATH"
+		grep -wi "CRON" "$FILEPATH" > out_logs/cron_syslog.txt
 else 
 	echo "file $FILEPATH NOT FOUND :-(" 
 fi
--- var_log.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-ls -la /var/log
-PAUSE=5
-echo "Waiting $PAUSE sec"
-read -t  $PAUSE
-exit 0
